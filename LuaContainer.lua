a92c3dd5bb6b69e4d5b8d7c3056de8c74f947f92
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-11-04 22:27:11 +0900

    Update LuaContainer.lua

--- LuaContainer.lua
+++ LuaContainer.lua
@@ -1,212 +1,99 @@
 -- LuaContainers.lua
--- Download this file and require as you want!
+-- 업그레이드 버전: 기존의 List, Stack, Queue, Deque, BinaryTree 컨테이너에 보완 기능을 추가했습니다.
 
--- List implementation
-List = {}
-
-function List:new()
-    local list = {elements = {}}
-    setmetatable(list, self)
-    self.__index = self
-    return list
-end
-
-function List:insert(value)
-    table.insert(self.elements, value)
-end
-
-function List:remove(index)
-    table.remove(self.elements, index)
-end
-
-function List:clear()
-    self.elements = {}
-end
-
-function List:check()
-    print("The length of List : " .. #self.elements)
-    for i = 1, #self.elements do
-        print(self.elements[i])
+-- List 보완
+function List:find(value)
+    for i, v in ipairs(self.elements) do
+        if v == value then
+            return i -- 찾은 값의 인덱스 반환
+        end
     end
+    return nil -- 값이 없을 경우 nil 반환
 end
 
--- Stack implementation
-Stack = {}
-
-function Stack:new()
-    local stack = {elements = {}}
-    setmetatable(stack, self)
-    self.__index = self
-    return stack
+function List:size()
+    return #self.elements
 end
 
-function Stack:push(value)
-    table.insert(self.elements, value)
-end
-
-function Stack:pop()
-    return table.remove(self.elements)
-end
-
-function Stack:clear()
-    self.elements = {}
-end
-
-function Stack:check()
-    print("The length of List : " .. #self.elements)
-    print("'TOP' element in Stack : " .. #self.elements)
-    for i = 1, #self.elements do
-        print(self.elements[i])
-    end
-end
-
--- Queue implementation
-Queue = {}
-
-function Queue:new()
-    local queue = {elements = {}}
-    setmetatable(queue, self)
-    self.__index = self
-    return queue
-end
-
-function Queue:enqueue(value)
-    table.insert(self.elements, value)
-end
-
-function Queue:dequeue()
-    return table.remove(self.elements, 1)
-end
-
-function Queue:isEmpty()
+function List:isEmpty()
     return #self.elements == 0
 end
 
-function Queue:clear()
-    self.elements = {}
+-- Stack 보완
+function Stack:peek()
+    return self.elements[#self.elements] -- Stack의 최상단 요소 확인
 end
 
-function Queue:check()
-    print("The length of List : " .. #self.elements)
-    for i = 1, #self.elements do
-        print(self.elements[i])
+function Stack:size()
+    return #self.elements
+end
+
+function Stack:isEmpty()
+    return #self.elements == 0
+end
+
+-- Queue 보완
+function Queue:peek()
+    return self.elements[1] -- Queue의 첫 번째 요소 확인
+end
+
+function Queue:size()
+    return #self.elements
+end
+
+-- Deque 보완
+function Deque:peekFront()
+    return self.elements[self.first] -- Deque의 앞쪽 요소 확인
+end
+
+function Deque:peekBack()
+    return self.elements[self.last] -- Deque의 뒤쪽 요소 확인
+end
+
+function Deque:size()
+    return self.last - self.first + 1
+end
+
+function Deque:isEmpty()
+    return self.first > self.last
+end
+
+-- BinaryTree 보완
+function BinaryTree:find(value)
+    return self:_findNode(self.root, value)
+end
+
+function BinaryTree:_findNode(node, value)
+    if not node then
+        return false -- 노드가 없으면 false 반환
     end
-end
-
--- Deque implementation
-Deque = {}
-
-function Deque:new()
-    local deque = {first = 0, last = -1, elements = {}}
-    setmetatable(deque, self)
-    self.__index = self
-    return deque
-end
-
-function Deque:pushFront(value)
-    local first = self.first - 1
-    self.first = first
-    self.elements[first] = value
-end
-
-function Deque:pushBack(value)
-    local last = self.last + 1
-    self.last = last
-    self.elements[last] = value
-end
-
-function Deque:popFront()
-    local first = self.first
-    if first > self.last then
-        return nil
-    end
-    local value = self.elements[first]
-    self.elements[first] = nil -- to allow garbage collection
-    self.first = first + 1
-    return value
-end
-
-function Deque:popBack()
-    local last = self.last
-    if self.first > last then
-        return nil
-    end
-    local value = self.elements[last]
-    self.elements[last] = nil -- to allow garbage collection
-    self.last = last - 1
-    return value
-end
-
-function Deque:clear()
-    self.first = 0
-    self.last = -1
-    self.elements = {}
-end
-
-function Deque:check()
-    print("The length of List : " .. #self.elements)
-    for i = 1, #self.elements do
-        print(self.elements[i])
-    end
-end
-
--- TreeNode implementation
-TreeNode = {}
-
-function TreeNode:new(value)
-    local node = {value = value, left = nil, right = nil}
-    setmetatable(node, self)
-    self.__index = self
-    return node
-end
-
--- BinaryTree implementation
-BinaryTree = {}
-
-function BinaryTree:new()
-    local tree = {root = nil}
-    setmetatable(tree, self)
-    self.__index = self
-    return tree
-end
-
-function BinaryTree:insert(value)
-    if not self.root then
-        self.root = TreeNode:new(value)
+    if node.value == value then
+        return true -- 값을 찾으면 true 반환
+    elseif value < node.value then
+        return self:_findNode(node.left, value)
     else
-        self:_insertNode(self.root, value)
+        return self:_findNode(node.right, value)
     end
 end
 
-function BinaryTree:_insertNode(node, value)
-    if value < node.value then
-        if not node.left then
-            node.left = TreeNode:new(value)
-        else
-            self:_insertNode(node.left, value)
-        end
-    else
-        if not node.right then
-            node.right = TreeNode:new(value)
-        else
-            self:_insertNode(node.right, value)
-        end
+function BinaryTree:height()
+    return self:_nodeHeight(self.root)
+end
+
+function BinaryTree:_nodeHeight(node)
+    if not node then
+        return 0
     end
+    local leftHeight = self:_nodeHeight(node.left)
+    local rightHeight = self:_nodeHeight(node.right)
+    return math.max(leftHeight, rightHeight) + 1
 end
 
-function BinaryTree:traverseInOrder(callback)
-    self:_traverseInOrder(self.root, callback)
+function BinaryTree:isEmpty()
+    return self.root == nil
 end
 
-function BinaryTree:_traverseInOrder(node, callback)
-    if node then
-        self:_traverseInOrder(node.left, callback)
-        callback(node.value)
-        self:_traverseInOrder(node.right, callback)
-    end
-end
-
--- LuaContainers table containing all container types
+-- LuaContainers 테이블
 LuaContainers = {List = List, Stack = Stack, Queue = Queue, Deque = Deque, BinaryTree = BinaryTree}
 
 return LuaContainers
